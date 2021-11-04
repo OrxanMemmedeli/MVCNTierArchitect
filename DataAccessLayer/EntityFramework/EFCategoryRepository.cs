9b4445847613b1faa8b6dc8bf298dcc77601dda5
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-11-04 18:52:58 +0400

    odev 2

--- DataAccessLayer/EntityFramework/EFCategoryRepository.cs
+++ DataAccessLayer/EntityFramework/EFCategoryRepository.cs
@@ -1,8 +1,10 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +13,12 @@ namespace DataAccessLayer.EntityFramework
 {
     public class EFCategoryRepository : GenericRepository<Category>, ICategoryDal
     {
+        public List<Category> GetAllWithHeading()
+        {
+            using (var context = new MVCContext())
+            {
+                return context.Categories.Include(x => x.Headings).ToList();
+            }
+        }
     }
 }
