5145f4b18767723fd885671b4a3cf0e18e865e4c
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-11-09 14:36:43 +0400

    Writer Controller Part 2

--- MVCNTierArchitect/Areas/Admin/Controllers/WriterController.cs
+++ MVCNTierArchitect/Areas/Admin/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +14,82 @@ namespace MVCNTierArchitect.Areas.Admin.Controllers
     public class WriterController : Controller
     {
         private readonly WriterManager _writerManager;
+        private readonly WriterValidator _validator;
 
         public WriterController()
         {
             _writerManager = new WriterManager(new EFWriterRepository());
+            _validator = new WriterValidator();
         }
 
         public ActionResult Index()
         {
             var writers = _writerManager.GetAll();
             return View(writers);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Writer writer)
+        {
+            ValidationResult results = _validator.Validate(writer);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(writer);
+            }
+            _writerManager.Add(writer);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            var Writer = _writerManager.GetByID(x => x.ID == id);
+            if (Writer == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            return View(Writer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Writer writer)
+        {
+            if ((writer.Password == "Test123456!!" || string.IsNullOrEmpty(writer.Password)) && (writer.ConfirmPassword == "Test123456!!" || string.IsNullOrEmpty(writer.ConfirmPassword)))
+            {
+                writer.Password = writer.OldPassword;
+                writer.ConfirmPassword = writer.OldPassword;
+            }
+
+            ValidationResult results = _validator.Validate(writer);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(writer);
+            }
+
+            _writerManager.Update(writer);
+            return RedirectToAction("Index");
         }
     }
 }
