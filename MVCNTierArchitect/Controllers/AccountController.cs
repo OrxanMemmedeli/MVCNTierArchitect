b6131cbcf19662865e8b27697b63b7ac6a40c742
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-01-07 17:36:30 +0400

    finish lesson 71

--- MVCNTierArchitect/Controllers/AccountController.cs
+++ MVCNTierArchitect/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Tools.Abstract;
 
 namespace MVCNTierArchitect.Controllers
@@ -61,7 +62,8 @@ namespace MVCNTierArchitect.Controllers
 
             if (admin != null)
             {
-                //other prosses
+                FormsAuthentication.SetAuthCookie(admin.UserName, false);
+                Session["AdminUserName"] = admin.UserName;
                 if (!string.IsNullOrEmpty(returnURL))
                 {
                     return Redirect(returnURL);
