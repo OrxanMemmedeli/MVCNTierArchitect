10fcbd8fb07e25db31dbab24287c69cb3012f626
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-03-11 13:02:50 +0400

    Using API For Showcase (Front part 1)

--- MVCNTierArchitect/Controllers/DefaultController.cs
+++ MVCNTierArchitect/Controllers/DefaultController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using Newtonsoft.Json;
+using ShowcaseAPI.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,10 +14,28 @@ namespace MVCNTierArchitect.Controllers
     [AllowAnonymous]
     public class DefaultController : Controller
     {
+        private readonly IAdressService _adressService;
+
+        public DefaultController(IAdressService adressService)
+        {
+            _adressService = adressService;
+        }
         // GET: Default
         public ActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> Tools()
+        {
+            var url = _adressService.GetLast();
+
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync(url.URL + "api/Tool");
+            var jsonstring = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.SerializeObject(jsonstring);
+            return Json(values);
+
         }
     }
 }
