8f2a8fc4e22d77868d0b0c4c1d2fb34ecea301df
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-03-13 18:20:59 +0400

    Finish Showcase Front side

--- BusinessLayer/ValidationRules/ContactValidator.cs
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -15,8 +15,9 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.UserName).NotEmpty().WithMessage("Istifadeci Adi boş ola bilməz")
                 .MinimumLength(3).WithMessage("Istifadeci Adi minimum 3 simvol ola biler")
                 .MaximumLength(50).WithMessage("Istifadeci Adi maksimum 50 simvol ola biler");
-            RuleFor(x => x.Email).MaximumLength(250).WithMessage("Email Yolu maksimum 250 simvol ola biler")
-                .EmailAddress().WithMessage("Email adresi duzgen daxil edilmeyib");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email adresi boş ola bilməz")
+                .MaximumLength(250).WithMessage("Email adresi maksimum 250 simvol ola biler")
+                .EmailAddress().WithMessage("Email adresi düzgün daxil edilmeyib");
             RuleFor(x => x.Subject).NotEmpty().WithMessage("Basliq boş ola bilməz")
                 .MaximumLength(50).WithMessage("Basliq maksimum 50 simvol ola biler");
             RuleFor(x => x.Message).NotEmpty().WithMessage("Mesaj boş ola bilməz");
