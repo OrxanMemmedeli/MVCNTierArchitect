4fd0ae7c30fadf14e662357fa8b31f47bb3d0350
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-03-24 18:40:58 +0400

    login page add google Recaptcha

--- MVCNTierArchitect/Models/ViewModels/LoginViewModel.cs
+++ MVCNTierArchitect/Models/ViewModels/LoginViewModel.cs
@@ -9,5 +9,6 @@ namespace MVCNTierArchitect.Models.ViewModels
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string Captcha { get; set; }
     }
 }
